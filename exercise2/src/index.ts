ae39fbe716e3ea6ee9700748256e84bc8bfcefd0
dave.rodriguez <dave.rodriguez@hansoninc.com>
2024-05-16 15:11:42 -0400

    Initial commit of exercises

--- exercise2/src/index.ts
+++ exercise2/src/index.ts
@@ -1,11 +1,11 @@
-let article: {
-  author: string;
-  content: string;
-  title: string;
-  image?: string;
-};
-article = {
-  author: "Stella",
-  content: "Hello World",
-  title: "My First Article",
-};
+const airplane = {
+  model: "Airbus A380",
+  flightNumber: "A2201",
+  timeOfDeparture: new Date(),
+  timeOfArrival: new Date(),
+  caterer: {
+    name: "Special Food Ltd",
+    address: "484, Some Street, New York",
+    phone: 1452125
+  }
+}
